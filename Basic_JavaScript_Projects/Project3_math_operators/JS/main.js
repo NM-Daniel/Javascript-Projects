dcaff4cebba165867d7d3838c82a0cb3efe3d3d3
NM-Daniel <165437058+NM-Daniel@users.noreply.github.com>
2024-05-10 10:27:02 -0700

    multiply and divide

---
+++ Basic_JavaScript_Projects/Project3_math_operators/JS/main.js
@@ -0,0 +1,19 @@
+function addition_Function() {
+    var addition = 2 + 2;
+    document.getElementById("Math+").innerHTML = "2 + 2 = " + addition;
+}
+
+function subtraction_Function() {
+    var subtraction = 5 - 4;
+    document.getElementById("Math-").innerHTML = "5 - 4 = " + subtraction;
+}
+
+function multiplication() {
+    var simple_Math = 6 * 8;
+    document.getElementbyID("Math*").innerHTML = "6 * 8 = " + simple_Math;
+}
+
+function division() {
+    var div = 48 / 6;
+    document.getElementbyID("Math/").innerHTML = "48 / 6 = " + div;
+}
